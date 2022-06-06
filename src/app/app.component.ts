1adce949e9bfd939a5af999837ebc02191f29a55
Surya <Surya.G@ext.saint-gobain.com>
2022-06-06 15:20:39 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+declare const scanButton: any;
+declare const writeButton: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nfc-app';
+}
